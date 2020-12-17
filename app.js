ce178a340e3006f234dde051b44f289abac8db96
Muhammet Ali Yenidede <aliyenidede@gmail.com>
2020-12-17 18:58:20 +0300

    mongoDB added

--- app.js
+++ app.js
@@ -4,34 +4,44 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 const hostname = '127.0.0.1'
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1/nodeblog_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
 
 app.use(express.static('assets'))
 
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars')
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.render('site/index')
 })
 
-app.get('/blog',(req, res) => {
+app.get('/blog', (req, res) => {
     res.render('site/blog')
 })
 
-app.get('/contact',(req, res) => {
+app.get('/contact', (req, res) => {
     res.render('site/contact')
 })
 
-app.get('/about',(req, res) => {
+app.get('/about', (req, res) => {
     res.render('site/about')
 })
 
-app.get('/login',(req, res) => {
+app.get('/login', (req, res) => {
     res.render('site/login')
 })
 
-app.get('/register',(req, res) => {
+app.get('/register', (req, res) => {
     res.render('site/register')
 })
 
-app.listen(port, hostname, ()=>{ console.log(`server working now, http://${hostname}:${port}/`)})
+app.listen(port, hostname, () => {
+    console.log(`server working now, http://${hostname}:${port}/`)
+})
